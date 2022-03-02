29d765e3be5bc75fd996022b358b56b275683d8f
juliafisher <julia.fisher@detroitlabs.com>
2022-03-02 11:52:13 -0500

    Added cookies and macaroons class

--- src/com/company/Cookies.java
+++ src/com/company/Cookies.java
@@ -1,4 +1,31 @@
 package com.company;
 
 public class Cookies extends BakedGoods{
+    private int numOfCookies;
+    private String flavors;
+    private double price;
+
+    public int getNumOfCookies() {
+        return numOfCookies;
+    }
+
+    public String flavors() {
+        return flavors;
+    }
+
+    public double price() {
+        return price;
+    }
+    public Cookies(int numOfCookies, String flavors, double price, boolean freshDaily, boolean isGlutenFree){
+        super(freshDaily, isGlutenFree);
+        this.numOfCookies = numOfCookies;
+        this.flavors = flavors;
+        this.price = price;
+    }
+
+    @Override
+    void Print() {
+        super.Print();
+        System.out.println("This cookie is the following" + numOfCookies + '\n' + flavors + '\n' + +'\n' + price);
+    }
 }
--- src/com/company/Macaroons.java
+++ src/com/company/Macaroons.java
@@ -1,4 +1,37 @@
 package com.company;
 
-public class Macaroons extends BakedGoods{
+public class Macaroons extends BakedGoods {
+    private int numOfMacaroons;
+    private String flavors;
+    private double price;
+    private String color;
+
+    public int getNumOfMacaroon() {
+        return numOfMacaroons;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getFlavors() {
+        return flavors;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Macaroons(int numOfMacaroons, String flavors, double price, boolean freshDaily, boolean isGlutenFree) {
+        super(freshDaily, isGlutenFree);
+        this.numOfMacaroons = numOfMacaroons;
+        this.flavors = flavors;
+        this.price = price;
+    }
+
+    @Override
+    void Print() {
+        super.Print();
+        System.out.println("This macaroon is the following" + numOfMacaroons +'\n' + color+ '\n' + flavors + '\n' + +'\n' + price);
+    }
 }
