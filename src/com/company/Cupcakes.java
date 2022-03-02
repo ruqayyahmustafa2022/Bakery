14f45ac6f330963d2183547dc58c1df5578c9abc
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-03-02 10:01:31 -0700

    Adding cupcakes and People class

--- src/com/company/Cupcakes.java
+++ src/com/company/Cupcakes.java
@@ -1,4 +1,32 @@
 package com.company;
 
 public class Cupcakes extends BakedGoods{
+    private int numOfCupcakes;
+    private String flavors;
+    private int price;
+
+    public int getNumOfCupcakes() {
+        return numOfCupcakes;
+    }
+
+    public String getFlavors() {
+        return flavors;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Cupcakes (int numOfCupcakes, String flavors, int price, boolean freshDaily, boolean isGlutenFree) {
+        super(freshDaily, isGlutenFree);
+        this.numOfCupcakes = numOfCupcakes;
+        this.flavors = flavors;
+        this.price = price;
+    }
+
+    @Override
+    void Print() {
+        super.Print();
+        System.out.println("This cupcake is the following" + numOfCupcakes + '\n' + flavors + '\n' + price);
+    }
 }
--- src/com/company/People.java
+++ src/com/company/People.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public class People {
+    private String title; // customer or cashier
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public People (String title, String name) {
+        this.title = title;
+        this.name = name;
+    }
 }
